a031d3a6f98e1da7c5e3b85512adbcd7c4c6cc40
Ola Smaaberg <ola.smaaberg@instech.no>
2023-12-05 13:38:49 +0100

    e

--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -5,33 +5,39 @@ fn main() {
 }
 
 fn part1 (input: &str) -> i32 {
-
   let answer: i32 = input.lines().fold(0, |acc: i32, line: &str| {
-    let nums = line.char_indices().fold("".to_string(), |acc, (i, c)| {
+    let mut index = 0;
+    let mut mul:i32 = 10;
+    let nums = line.char_indices().fold([872377 as i32;2], |mut acc, (i, c)| {
+      if acc[0] != 872377 {
+        index = 1;
+        mul = 1;
+      }
       if c.is_digit(10) {
-        acc + &c.to_string()
-      } else {
-        let mut value = "";
+          acc[index] = i as i32*mul;
+          acc
+      }
+      else {
         let substring = &line[i..];
-        if substring.starts_with("zero") {value = "0"}
-        else if substring.starts_with("one")  {value = "1"}
-        else if substring.starts_with("two")  {value = "2"}
-        else if substring.starts_with("three"){value = "3"}
-        else if substring.starts_with("four") {value = "4"}
-        else if substring.starts_with("five") {value = "5"}
-        else if substring.starts_with("six")  {value = "6"}
-        else if substring.starts_with("seven"){value = "7"}
-        else if substring.starts_with("eight"){value = "8"}
-        else if substring.starts_with("nine") {value = "9"}
-        acc+value
+        if substring.starts_with("zero")      {acc[index] = 0 as i32*mul;}
+        else if substring.starts_with("one")  {acc[index] = 1 as i32*mul;}
+        else if substring.starts_with("two")  {acc[index] = 2 as i32*mul;}
+        else if substring.starts_with("three"){acc[index] = 3 as i32*mul;}
+        else if substring.starts_with("four") {acc[index] = 4 as i32*mul;}
+        else if substring.starts_with("five") {acc[index] = 5 as i32*mul;}
+        else if substring.starts_with("six")  {acc[index] = 6 as i32*mul;}
+        else if substring.starts_with("seven"){acc[index] = 7 as i32*mul;}
+        else if substring.starts_with("eight"){acc[index] = 8 as i32*mul;}
+        else if substring.starts_with("nine") {acc[index] = 9 as i32*mul;}
+        acc
       }
     });
-    let partial: i32 = (format!("{}{}",nums.chars().next().unwrap(), nums.chars().last().unwrap())).parse::<i32>().unwrap();
+    print!("{:?} ", nums);
+    let partial: i32 = nums.iter().sum::<i32>();
     acc + partial
   });
   answer
 }
-
 #[cfg(test)]
 mod tests {
   use super::*;
--- day-01/target/debug/deps/part2-abb32c0515152e50.d
+++ day-01/target/debug/deps/part2-abb32c0515152e50.d
@@ -1,6 +1,7 @@
-/home/olafsm/instech/aoc2023/day-01/target/debug/deps/part2-abb32c0515152e50: src/bin/part2.rs src/bin/test.txt
+/home/olafsm/instech/aoc2023/day-01/target/debug/deps/part2-abb32c0515152e50: src/bin/part2.rs src/bin/input.txt src/bin/test.txt
 
-/home/olafsm/instech/aoc2023/day-01/target/debug/deps/part2-abb32c0515152e50.d: src/bin/part2.rs src/bin/test.txt
+/home/olafsm/instech/aoc2023/day-01/target/debug/deps/part2-abb32c0515152e50.d: src/bin/part2.rs src/bin/input.txt src/bin/test.txt
 
 src/bin/part2.rs:
+src/bin/input.txt:
 src/bin/test.txt:
